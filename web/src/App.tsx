17895fd856b1f86cfd654c49e798a64bf9c9c4df
thatretrodev <thatretrodev@outlook.com>
2022-07-15 09:34:27 -0400

    Modify page again

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,8 +8,8 @@ function App() {
 	return (
 		<>
 			<IconButton style={{marginTop: 10, marginLeft: 10}} aria-label="Toggle theme" colorScheme={colorMode == "light" ? "blue" : undefined} onClick={toggleColorMode} icon={colorMode == "light" ? (<MoonIcon />) : (<SunIcon />)} />
-			<Center width="100%" height="100%">
-				<Button>Hello, World!</Button>	
+			<Center width="100%">
+				<Button colorScheme={colorMode == "light" ? "blue" : undefined}>Hello, World!</Button>	
 			</Center>
 		</>
 	);
