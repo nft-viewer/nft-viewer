16af1abb18e0bcc6d8f96153d13f6b7c3a193346
thatretrodev <thatretrodev@outlook.com>
2022-07-15 09:20:48 -0400

    Add Chakra UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { IconButton, Center, useColorMode } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 function App() {
+	const { colorMode, toggleColorMode } = useColorMode();
+	
 	return (
-		<>
-			<h1>Hello, World!</h1>
-		</>
+		<Center width="100vw" height="100vh">
+			<IconButton aria-label="Toggle theme" colorScheme={colorMode == "light" ? "blue" : undefined} onClick={toggleColorMode} icon={colorMode == "light" ? (<MoonIcon />) : (<SunIcon />)} />
+		</Center>
 	);
 }
 
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider theme={extendTheme({config: {initialColorMode: "dark", useSystemColorMode: false}})}>
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>
 );
