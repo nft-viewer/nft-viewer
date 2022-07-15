504721e599c2267c8241f511885c88fa1519cd2e
thatretrodev <thatretrodev@outlook.com>
2022-07-15 10:16:07 -0400

    Add thirdweb

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Button, IconButton, Center, useColorMode } from '@chakra-ui/react';
-import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
 function App() {
 	const { colorMode, toggleColorMode } = useColorMode();
+
+	const connectWithMetamask = useMetamask();
+	const disconnectWithMetamask = useDisconnect();
+	const address = useAddress();
 	
 	return (
 		<>
 			<IconButton style={{marginTop: 10, marginLeft: 10}} aria-label="Toggle theme" colorScheme={colorMode == "light" ? "blue" : undefined} onClick={toggleColorMode} icon={colorMode == "light" ? (<MoonIcon />) : (<SunIcon />)} />
 			<Center width="100%">
-				<Button colorScheme={colorMode == "light" ? "blue" : undefined}>Hello, World!</Button>	
+				{address ? (
+					<div>
+						<p>{address}</p>
+						<Button style={{marginTop: 10}} colorScheme={colorMode == "light" ? "blue" : undefined} onClick={disconnectWithMetamask}>Disconnect wallet</Button>
+					</div>
+				) : (
+					<>
+						<Button colorScheme={colorMode == "light" ? "blue" : undefined} onClick={connectWithMetamask}>Connect wallet</Button>
+					</>
+				)}
 			</Center>
 		</>
 	);
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider theme={extendTheme({config: {initialColorMode: "dark", useSystemColorMode: false}})}>
-			<App />
-		</ChakraProvider>
+		<ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+			<ChakraProvider theme={extendTheme({config: {initialColorMode: "dark", useSystemColorMode: false}})}>
+				<App />
+			</ChakraProvider>
+		</ThirdwebProvider>
 	</React.StrictMode>
 );
